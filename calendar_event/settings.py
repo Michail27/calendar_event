968bc8cd2f6a249cca9c7d1221a5693012775953
Michail27 <michail27.03@mail.ru>
2021-02-05 11:21:46 +0300

    Change password email

--- calendar_event/settings.py
+++ calendar_event/settings.py
@@ -128,11 +128,11 @@ USE_L10N = True
 USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST_USER = "michail27.03@gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'michail27.03.1988'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 # Static files (CSS, JavaScript, Images)
