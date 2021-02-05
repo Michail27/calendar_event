8c1018e024ea1670ccf6c8346e4dd4364c9f91d0
Michail27 <michail27.03@mail.ru>
2021-02-05 10:59:28 +0300

    Celery finish

--- calendar_event/settings.py
+++ calendar_event/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'calendar_event.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         "USER": os.environ.get("POSTGRES_USER"),
-#         'NAME': os.environ.get("POSTGRES_DB"),
-#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#         "HOST": os.environ.get("POSTGRES_HOST"),
-#         "PORT": os.environ.get("POSTGRES_PORT"),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "USER": os.environ.get("POSTGRES_USER"),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    }
+}
 
 
 # Password validation
